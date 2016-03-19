6db78186fdf085d7b44e02cc7b4ce7110637a859
Christine Tacha <tacha.christine@gmail.com>
2016-03-19 12:30:19 -0700

    grab JSON object

--- js/git.js
+++ js/git.js
@@ -1,10 +1,1 @@
-var apiKey = require("./../.env").apiKey;
 
-exports.getRepos = function(input) {
-  $.get('https://api.github.com/users/' + input + '?access_token=' + e411700fa34809cd89e67c21d61c7e99a31776ad).then(function(response) {
-    var userName = response.name;
-    console.log(userName);
-  }).fail(function(error) {
-    console.log(error.responseJSON.message);
-  });
-};
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,4 +1,29 @@
+$(function() {
+  console.log(exports.getRepos());
 
-$(document).ready(function() {
-  console.log("ready!");
 });
+
+var apiKey = require('./../.env').apiKey;
+
+exports.getRepos = function(){
+  // $.get('https://api.github.com/users/daneden?access_token=e411700fa34809cd89e67c21d61c7e99a31776ad').then(function(response){
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+
+
+
+
+
+// $(document).ready(function() {
+//   var userName = document.getElementById('enterUsername');
+//   var userArray = [];
+//   $('form#getRepos').submit(function(event) {
+//     $.getRepos('userName');
+//   });
+//   console.log("got it!");
+// });
