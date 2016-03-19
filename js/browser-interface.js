7f8dd21c5be006dac6b9575fd560b9ea32eec641
Christine Tacha <tacha.christine@gmail.com>
2016-03-19 12:41:24 -0700

    add a different API call to access user's pulic repos

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,4 +1,4 @@
 $(function() {
-  console.log(exports.getRepos());
+  console.log(exports.getRepos('christinetacha'));
 
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,13 +1,13 @@
 $(function() {
-  console.log(exports.getRepos());
+  console.log(exports.getRepos('christinetacha'));
 
 });
 
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
+exports.getRepos = function(gitUser){
   // $.get('https://api.github.com/users/daneden?access_token=e411700fa34809cd89e67c21d61c7e99a31776ad').then(function(response){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/'+ gitUser +'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
